c81ed0bb7171de8d49cfa9ab8631a182ed7c0526
Cesar <50472267+EmilCes@users.noreply.github.com>
2024-05-31 08:21:34 -0600

    User Controller Tests

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ app.use(errorlogger, errorhandler);
 
 
 // Start web server  SERVER_PORT
-app.listen(process.env.SERVER_PORT, () => {
+const server = app.listen(process.env.SERVER_PORT, () => {
     console.log(`Effort Backend listening: ${process.env.SERVER_PORT}`)
 });
+
+module.exports = server
